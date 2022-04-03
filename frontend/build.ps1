88c378484522abf3b2eca9537bb42aa0940fbd1b
东方盛夏 <2667647118@qq.com>
2022-04-03 13:45:10 +0800

    add prebuilts

--- frontend/build.ps1
+++ frontend/build.ps1
@@ -1,19 +1,19 @@
 $dest = $env:USERPROFILE + '\AppData\Local\electron-builder\Cache\wincodesign\wincodesign-2.6.0'
-$src = 'prebuilt\wincodesign\wincodesign-2.6.0'
+$src = 'prebuilt\wincodesign\wincodesign-2.6.0.zip'
 if (-not (Test-Path $dest)) {
-    Copy-Item -Path $src -Destination $dest -Recurse -Force
+    Expand-Archive -Path $src -DestinationPath $dest
 }
 
 $dest = $env:USERPROFILE + '\AppData\Local\electron-builder\Cache\nsis\nsis-3.0.4.1'
-$src = 'prebuilt\nsis\nsis-3.0.4.1'
+$src = 'prebuilt\nsis\nsis-3.0.4.1.zip'
 if (-not (Test-Path $dest)) {
-    Copy-Item -Path $src -Destination $dest -Recurse -Force
+    Expand-Archive -Path $src -DestinationPath $dest
 }
 
 $dest = $env:USERPROFILE + '\AppData\Local\electron-builder\Cache\nsis\nsis-resources-3.4.1'
-$src = 'prebuilt\nsis\nsis-resources-3.4.1'
+$src = 'prebuilt\nsis\nsis-resources-3.4.1.zip'
 if (-not (Test-Path $dest)) {
-    Copy-Item -Path $src -Destination $dest -Recurse -Force
+    Expand-Archive -Path $src -DestinationPath $dest
 }
 
 $destDir = $env:USERPROFILE + '\AppData\Local\electron\cache\'
